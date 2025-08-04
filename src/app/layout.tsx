b012045c742e7f41967ed061e41096291114e6f2
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-08-04 19:49:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      secretKey={process.env.CLERK_SECRET_KEY}
+    >
       <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
