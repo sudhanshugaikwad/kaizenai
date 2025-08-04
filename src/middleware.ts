be30a2e213fed2b4c993348074f337b0c60fbe50
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-08-04 21:52:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -8,9 +9,9 @@ export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
     auth().protect();
   }
-}, { 
-  publishableKey: "pk_test_c2luZ3VsYXItYmFzaWxpc2stNTcuY2xlcmsuYWNjb3VudHMuZGV2JA",
-  secretKey: "sk_test_nSGoft6sZ8hKRRAfbrJ7DqUgGkam9YzLb1Q3cpkJ5O"
+}, {
+  publishableKey: 'pk_test_c2luZ3VsYXItYmFzaWxpc2stNTcuY2xlcmsuYWNjb3VudHMuZGV2JA',
+  secretKey: 'sk_test_nSGoft6sZ8hKRRAfbrJ7DqUgGkam9YzLb1Q3cpkJ5O'
 });
 
 export const config = {
