0aab68d2bf357bee838b7058edf49443962822fd
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-08-04 21:40:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      secretKey={process.env.CLERK_SECRET_KEY}
-    >
+    <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
@@ -35,4 +32,3 @@ export default function RootLayout({
       </html>
     </ClerkProvider>
   );
-}
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
     auth().protect();
   }
-});
+}, { publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
