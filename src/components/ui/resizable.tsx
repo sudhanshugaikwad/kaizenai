4b58300bb40e3496359ad48e1ac5ddb106f9114f
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-08-18 07:41:02 +0000

    [13:08:46.961] Running build in Washington, D.C., USA (East) – iad1 [13:

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,12 +1,18 @@
 "use client"
 
 import * as React from "react"
-import { Panel, PanelGroup, PanelGroupOnLayout, PanelResizeHandle } from "react-resizable-panels"
+import {
+  Panel,
+  PanelGroup,
+  type ImperativePanelGroupHandle,
+  type ImperativePanelHandle,
+  PanelResizeHandle,
+} from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroup = React.forwardRef<
-  HTMLDivElement,
+  ImperativePanelGroupHandle,
   React.ComponentProps<typeof PanelGroup> & {
     className?: string
   }
@@ -33,6 +39,7 @@ const ResizableHandle = React.forwardRef<
 >(({ withHandle, className, ...props }, ref) => {
   return (
     <PanelResizeHandle
+      ref={ref}
       className={cn(
         "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=horizontal]]:focus-visible:ring-offset-[1px] [&[data-panel-group-direction=vertical]]:focus-visible:ring-offset-[1px]",
         className
