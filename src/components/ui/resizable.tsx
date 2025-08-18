a5830d2cca7eed9d2c3be5ab86a35e38746d10c0
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-08-18 07:23:49 +0000

    Error: Element type is invalid: expected a string (for built-in componen

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -21,14 +20,12 @@ const ResizablePanelGroup = ({
 
 const ResizablePanel = Panel
 
-const ResizableHandle = ({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof PanelGroup.Handle> & {
-  withHandle?: boolean
-}) => (
+const ResizableHandle = React.forwardRef<
+  React.ElementRef<typeof PanelGroup.Handle>,
+  React.ComponentProps<typeof PanelGroup.Handle> & { withHandle?: boolean }
+>(({ withHandle, className, ...props }, ref) => (
   <PanelGroup.Handle
+    ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=horizontal]]:focus-visible:ring-offset-[1px] [&[data-panel-group-direction=vertical]]:focus-visible:ring-offset-[1px]",
       className
@@ -39,6 +36,8 @@ const ResizableHandle = ({
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border" />
     )}
   </PanelGroup.Handle>
-)
+));
+ResizableHandle.displayName = "ResizableHandle";
+
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
