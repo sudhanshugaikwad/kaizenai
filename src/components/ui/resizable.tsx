b774ddaee960c99e9afdfe0347375157f5c2f5e1
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-08-18 07:24:38 +0000

    Error: Element type is invalid: expected a string (for built-in componen

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,43 +1,51 @@
 "use client"
 
 import * as React from "react"
-import { ImperativePanelHandle, Panel, PanelGroup } from "react-resizable-panels"
+import { Panel, PanelGroup, PanelGroupOnLayout, PanelResizeHandle } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
-const ResizablePanelGroup = ({
-  className,
-  ...props
-}: React.ComponentProps<typeof PanelGroup>) => (
+const ResizablePanelGroup = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<typeof PanelGroup> & {
+    className?: string
+  }
+>(({ className, ...props }, ref) => (
   <PanelGroup
+    ref={ref}
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
     )}
     {...props}
   />
-)
+))
+ResizablePanelGroup.displayName = "ResizablePanelGroup"
 
 const ResizablePanel = Panel
 
 const ResizableHandle = React.forwardRef<
-  React.ElementRef<typeof PanelGroup.Handle>,
-  React.ComponentProps<typeof PanelGroup.Handle> & { withHandle?: boolean }
->(({ withHandle, className, ...props }, ref) => (
-  <PanelGroup.Handle
-    ref={ref}
-    className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=horizontal]]:focus-visible:ring-offset-[1px] [&[data-panel-group-direction=vertical]]:focus-visible:ring-offset-[1px]",
-      className
-    )}
-    {...props}
-  >
-    {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border" />
-    )}
-  </PanelGroup.Handle>
-));
-ResizableHandle.displayName = "ResizableHandle";
+  ImperativePanelHandle,
+  React.ComponentProps<typeof PanelResizeHandle> & {
+    withHandle?: boolean
+    className?: string
+  }
+>(({ withHandle, className, ...props }, ref) => {
+  return (
+    <PanelResizeHandle
+      className={cn(
+        "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=horizontal]]:focus-visible:ring-offset-[1px] [&[data-panel-group-direction=vertical]]:focus-visible:ring-offset-[1px]",
+        className
+      )}
+      {...props}
+    >
+      {withHandle && (
+        <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border" />
+      )}
+    </PanelResizeHandle>
+  )
+})
+ResizableHandle.displayName = "ResizableHandle"
 
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
