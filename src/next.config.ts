7638851531f0bd0e235574a97d1f426adbb8e633
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-08-23 15:04:14 +0000

    Error: ./node_modules/@opentelemetry/context-async-hooks/build/src/Async

--- src/next.config.ts
+++ src/next.config.ts
@@ -43,6 +43,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
